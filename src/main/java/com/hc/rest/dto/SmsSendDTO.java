292250c0b59e4944f075d686feb2a5ebb794eebc
Jon Hatton <jhatton@jhatton-macbook.local>
2016-03-07 14:21:33 -0600

    Added send SMS modal

---
+++ src/main/java/com/hc/rest/dto/SmsSendDTO.java
@@ -0,0 +1,19 @@
+package com.hc.rest.dto;
+
+public class SmsSendDTO {
+
+	public SmsSendDTO() { }
+	
+	String phoneNumber;
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+
+}
